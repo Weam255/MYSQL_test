707431f96deae3af140a020df7bc068f628b1472
Weam255 <56806867+Weam255@users.noreply.github.com>
2019-12-08 12:26:32 +0200

    Add files via upload

---
+++ test_employee.sql
@@ -0,0 +1,4 @@
+
+iNSERT INTO `FirstSchema`.`Employee` (name_Employee, `salary_Employee`) VALUES ('KHALED', 555);
+DELETE FROM `FirstSchema`.`Employee` WHERE `id_Employee`='4';
+UPDATE `FirstSchema`.`Employee` SET `name_Employee`='nabil' WHERE `id_Employee`='3';
